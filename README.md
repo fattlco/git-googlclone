40f24ced6f839286bd2bb971532213a03b19ef8d
Tom Loh <fattlco@gmail.com>
2021-12-28 20:17:31 +0800

    add on Tuesday, 28-Dec-2021 @ 07:58 pm

---
+++ README.md
@@ -0,0 +1,21 @@
+Make a Google search engine clone: JavaScript PHP and MySQL
+============
+Udemy, 2021
+
+Learn JavaScript, PHP and MySQL by building your own search engine from scratch! A COMPLETE walkthrough!
+
+This REPO contain the source code for the project.
+
+| Mode          | LastWriteTime            | Length | Name     |                                                                 
+| ----          | -------------            | ------ | ----     |                                                                
+| d-----        | 28-Dec-2021   7:33 PM    |        |  Section-1-doodle-logo            |                                             
+| d-----        | 28-Dec-2021   7:48 PM    |        |  Section-2-doodle-activeTab       |                                           
+| d-----        | 28-Dec-2021   7:48 PM    |        |  Section-3-doodle-insertImages    |                                 
+| d-----        | 28-Dec-2021   8:04 PM    |        |  Section-4-doodle-termInSearchBox |                                                                          
+| d-----        | 28-Dec-2021   8:03 PM    |        |  Section-5-doodle-pagingEdgeCase  |                                         
+| d-----        | 28-Dec-2021   8:04 PM    |        |  Section-6-doodle-typeHiddenField |                                         
+| d-----        | 28-Dec-2021   8:03 PM    |        |  Section-7-doodle-finished        |   
+
+
+
+
---
+++ section-4-doodle-termInSearchBox/search.php
@@ -0,0 +1,112 @@
+<?php
+include("config.php");
+include("classes/SiteResultsProvider.php");
+
+if(isset($_GET["term"])) {
+	$term = $_GET["term"];
+}
+else {
+	exit("You must enter a search term");
+}
+
+$type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+
+
+	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Doodle</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper">
+	
+		<div class="header">
+
+
+			<div class="headerContent">
+
+				<div class="logoContainer">
+					<a href="index.php">
+						<img src="assets/images/doodleLogo.png">
+					</a>
+				</div>
+
+				<div class="searchContainer">
+
+					<form action="search.php" method="GET">
+
+						<div class="searchBarContainer">
+
+							<input class="searchBox" type="text" name="term" value="<?php echo $term; ?>">
+							<button class="searchButton">
+								<img src="assets/images/icons/search.png">
+							</button>
+						</div>
+
+					</form>
+
+				</div>
+
+			</div>
+
+
+			<div class="tabsContainer">
+
+				<ul class="tabList">
+
+					<li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+							Sites
+						</a>
+					</li>
+
+					<li class="<?php echo $type == 'images' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+							Images
+						</a>
+					</li>
+
+				</ul>
+
+
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+
+
+		<div class="mainResultsSection">
+
+			<?php
+			$resultsProvider = new SiteResultsProvider($con);
+
+			$numResults = $resultsProvider->getNumResults($term);
+
+			echo "<p class='resultsCount'>$numResults results found</p>";
+
+
+
+			echo $resultsProvider->getResultsHtml(1, 20, $term);
+			?>
+
+
+		</div>
+
+
+
+	</div>
+
+</body>
+</html>
